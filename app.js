dad966ee79f5e43d6d52afa06051af313ee8294f
hamidullo <ahamidullo248@gmail.com>
2025-02-06 15:06:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,94 @@
+// let num = prompt("enter a number");
+
+// variebles  var , let const
+
+// const num = 22;
+// console.log(num); // 22
+
+// num /= 2;
+// console.log(num); // 30
+
+// let num = 33;
+// console.log(num);
+
+// datatypes = primitive / non - primitive;
+// primitive = Number , String , Boolean  , Null , undefined , Bigint , Symbol
+
+// let num = false;
+
+// console.log(2  3);
+
+// let num;
+// console.log(num);
+
+// let number = 1212122122122222222222222222221202022202002002n;
+
+// console.log(typeof number);
+
+// non-primitive
+
+// Array
+// let Umidjon = {
+//   name: "Umidjon",
+//   age: 16,
+//   hobby: "Boks",
+//   pats: {
+//     cats: "Tiger",
+//     dog: "hatico",
+//   },
+// };
+// let arr2 = [23, "22", 43, [32, 0]];
+// console.log(typeof arr2);
+
+// function SayHello() {
+//   console.log("assalomu aleykum");
+// }
+
+// SayHello();
+// SayHello();
+
+// let num = 22;
+// let num1 = 88;
+
+// let result = num * num1;
+
+// function MultipleNums(num1, num2) {
+//   return num1 * num2;
+// }
+
+// let result = MultipleNums(21, 2);
+// console.log(result);
+
+// function MultipleNums(num1, num2) {
+//   return num1 * num2;
+// }
+// let MultipleNums = function (num1, num2) {
+//   return num1 * num2;
+// };
+
+// let MultipleNums = (num1, num2) => num1 * num2;
+
+// console.log(MultipleNums(12, 5));
+
+// DOM  document object module
+// Qo'sha olamiz
+// O'chira olamiz
+// O'zgartira olamiz
+
+// let box = document.querySelector(".box");
+
+// let box2 = document.createElement("h1");
+// box2.textContent = "Hello world!";
+// box2.classList.add("danger");
+// box2.classList.remove("danger");
+
+// box.innerHTML = "<h1> hello world!</h1>";
+// box.append(box2);
+
+
+
+let str = "hello world!";
+
+
+console.log(str);
+
